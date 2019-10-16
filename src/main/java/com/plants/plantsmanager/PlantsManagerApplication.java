c5d1c1b0e38aaf00930a8a949a6bb19c9d5e17ca
maja <majaferens@gmail.com>
2019-10-16 19:42:14 +0200

    Setup project

---
+++ src/main/java/com/plants/plantsmanager/PlantsManagerApplication.java
@@ -0,0 +1,14 @@
+package com.plants.plantsmanager;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantsManagerApplication.class, args);
+	}
+
+}
